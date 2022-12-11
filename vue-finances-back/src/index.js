5ed2940550568e51ce9105c164d3f0e495be1fc5
Igor Bitarães <igor_oliveira02@hotmail.com>
2022-12-11 10:41:23 -0300

    Alterações back

--- vue-finances-back/src/index.js
+++ vue-finances-back/src/index.js
@@ -1,17 +1,25 @@
-const { GraphQLServer} = require('graphql-yoga');
-const { prisma } = require('./generated/prisma-client');
+const { GraphQLServer } = require('graphql-yoga')
+const Binding = require('prisma-binding')
+const { prisma } = require('./generated/prisma-client')
 
-const resolvers = {
-  Query: {
-    user(parent, args, ctx, info){
-      return prisma.user({id: args.id});
-    }
-  }
-}
+const { endpoint, origin, playground, secret } = require('./config')
+const resolvers = require('./resolvers')
 
 const server = new GraphQLServer({
   typeDefs: `${__dirname}/schema.graphql`,
-  resolvers
+  resolvers,
+  context: request => ({
+    ...request,
+    db: new Binding.Prisma({
+      typeDefs: `${__dirname}/generated/graphql-schema/prisma.graphql`,
+      endpoint,
+      secret
+    }),
+    prisma
+  })
 })
 
-server.start().then(() => console.log('Server running on http://localhost:4000'))
+server.start({
+  cors: { origin },
+  playground
+}).then(() => console.log('Server running on http://localhost:4000...'))
